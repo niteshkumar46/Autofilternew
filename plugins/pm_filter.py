fa61cb954a0fbdb5ff1bed3239a2e39a989dbec7
RIO-NETWORK <139229666+RIO-NETWORK@users.noreply.github.com>
2023-07-20 19:56:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1398,7 +1398,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "filters":
         buttons = [[
-            InlinekeyboardButton('🎁More Features🎁',callback_data='modules2'),
+            InlinekeyboardButton('🎁More Features🎁',callback_data='modules_info'),
            ],[
             InlineKeyboardButton('Mᴀɴᴜᴀʟ FIʟᴛᴇʀ', callback_data='manuelfilter'),
             InlineKeyboardButton('Aᴜᴛᴏ FIʟᴛᴇʀ', callback_data='autofilter')
@@ -1437,7 +1437,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     
     elif query.data == "help":
         buttons = [[
-            InlinekeyboardButton('🎁More Features🎁',callback_data='modules2'),
+            InlinekeyboardButton('🎁More Features🎁',callback_data'modules_info'),
            ],[
             InlineKeyboardButton('FIʟᴛᴇʀs', callback_data='filters'),
             InlineKeyboardButton('Fɪʟᴇ Sᴛᴏʀᴇ', callback_data='store_file')
